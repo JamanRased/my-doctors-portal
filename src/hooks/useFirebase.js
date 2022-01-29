e6e423614c26e226a6eb524c5d415c600fce6745
JamanRased <jaman.rashed.199@gmail.com>
2022-01-29 22:47:05 +0600

    Update71.5

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,80 +1,54 @@
 import { useEffect, useState } from "react";
-import initializeFirebase from "../Pages/Login/Firebase/firebase.init";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,signOut, updateProfile } from "firebase/auth";
+import initializeFirebase from "../Pages/Login/Firebase/firebase.init";
 
 // initialize firebase app
 initializeFirebase();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    const [authError, setAuthError] = useState('');
-
     const auth = getAuth();
 
-     // Register Email Passwoard 
-     const registerUser = (email, password, name) => {
-        setIsLoading(true);
+    // Register Email Passwoard 
+     const registerUser = (email, password) =>{
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            setAuthError('');
-            const newUser = { email, displayName: name };
-            setUser(newUser);
-    
-            // send name to firebase after creation
-            updateProfile(auth.currentUser, {
-                displayName: name
-            }).then(() => {
-            }).catch((error) => {
-            });
+          // Signed in 
+          const user = userCredential.user;
+          // ...
         })
         .catch((error) => {
-            setAuthError(error.message);
-            console.log(error);
-        })
-        .finally(() => setIsLoading(false));
-}
- //Login Email Pass
- const loginUser = (email, password) => {
-    setIsLoading(true);
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            setAuthError('');
-        })
-        .catch((error) => {
-            setAuthError(error.message);
-        })
-        .finally(() => setIsLoading(false));
-}
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+     }
 
-// observe user state change
-useEffect(() => {
-    const unsubscribed = onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setUser(user);
-        } else {
-            setUser({})
-        }
-        setIsLoading(false);
-    });
-    return () => unsubscribed;
-}, [auth])
+    // observe user state change
+    useEffect(() => {
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser({})
+            }
+        });
+        return () => unsubscribed;
+    }, [auth])
 
     //Logout
     const logOut = () => {
-        setIsLoading(true);
-        signOut(auth)
-            .then(() => { })
-            .finally(() => setIsLoading(false));
+        signOut(auth).then(() => {
+            // Sign-out successful.
+          }).catch((error) => {
+            // An error happened.
+          });
     }
 
     return {
         user,
-        isLoading,
-        authError,
-        logOut,
-        loginUser,
         registerUser,
+        logOut
     }
 }
 
